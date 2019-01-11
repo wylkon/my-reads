979c7f284a3b13f5bbbcc26ba3bf0aba6575cf91
Wylkon Cardoso <wylkon@wylkon.com.br>
2019-01-11 17:21:12 -0200

    Criado página de erro

---
+++ src/pages/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SomeText from '../components/SomeText';
+
+const NoMatch = () => (
+  <SomeText className="grid-container">
+    <box-icon name="bot" animation="tada" size="lg" />
+    <p>404 Error - I think I'm lost F@#&!!</p>
+  </SomeText>
+);
+
+export default NoMatch;
